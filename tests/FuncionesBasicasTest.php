fe8d9e3e1541a87c4deb3d26734ba8065bc2cd8a
Jvtomas98 <jvtomas98@gmail.com>
2019-10-07 21:27:49 -0300

    Test para ambas clases

--- tests/FuncionesBasicasTest.php
+++ tests/FuncionesBasicasTest.php
@@ -3,11 +3,11 @@
 class FuncionesBasicasTest extends \PHPUnit\Framework\TestCase {
 
     public function crear(
-        $salario = 137455,
+        $salario = 10000,
         Array $montos = [],
         $nombre = "Fulano",
-        $apellido = "De Tal",
-        $dni = 41511603
+        $apellido = "deTal",
+        $dni = 11111111
     ) {
         $ca = new \App\EmpleadoEventual(
             $nombre,
@@ -19,5 +19,31 @@ class FuncionesBasicasTest extends \PHPUnit\Framework\TestCase {
         return $ca;
     }
 
+    public function testgetNombreApelldioFuncionaBien() {
+		$ca = $this->crear();
+		$this->assertEquals("Fulano deTal", ca->getNombreApellido());
+	}
+    public function testGetDNIFuncionaBien() {
+		$ca = $this->crear();
+		$this->assertEquals("11111111", ca->getDNI());
+    }
+    
+    public function testGetSalarioFuncionaBien() {
+		$ca = $this->crear();
+		$this->assertEquals(10000, ca->getSalario());
+    }
+    
+    public function testSetSectorFuncionaBien() {
+		$ca = $this->crear();
+		$this->assertEquals("Testing", ca->setSector());
+    }
+    public function testGetSectorFuncionaBien() {
+		$ca = $this->crear();
+		$this->assertEquals("No especificado", ca->getSector());
+    } //Falta terminar el test de __tostring 
+        public function testToString() {
+		$ca = $this->crear();
+		$this->assertEquals(, ca->__toString());
+    }
 
 }
