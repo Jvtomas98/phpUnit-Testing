434809348e72f95f719f20af92f842419329a053
Jvtomas98 <jv.tomas98@gmail.com>
2019-10-13 21:10:16 -0300

    All Funciones Basicas

--- tests/FuncionesBasicasTest.php
+++ tests/FuncionesBasicasTest.php
@@ -2,39 +2,48 @@
 
 abstract class FuncionesBasicasTest extends \PHPUnit\Framework\TestCase {
  
-	 public function testGetNombreApelldioFuncionaBien() {
-		$ca = $this->crear();
-		$this->assertEquals("Fulano De Tal", $ca->getNombreApellido());
-	 }
-	 public function testGetDNIFuncionaBien() {
-		$ca = $this->crear();
-		$this->assertEquals("11111111", $ca->getDNI());
-	    }
-    
-	 public function testGetSalarioFuncionaBien() {
-		$ca = $this->crear();
-		$this->assertEquals(10000, $ca->getSalario());
-  	  }
-    
-   	 public function testSetSectorFuncionaBien() {
-    	    $ca = $this->crear();
-    	    $ca->setSector("Informatica");
+    public function testGetNombreApelldioFuncionaBien() {
+      $ca = $this->crear();
+      $this->assertEquals("Fulano De Tal", $ca->getNombreApellido());
+    }
+	  public function testGetDNIFuncionaBien() {
+      $ca = $this->crear();
+      $this->assertEquals("11111111", $ca->getDNI());
+	  }
+    public function testGetSalarioFuncionaBien() {
+      $ca = $this->crear();
+      $this->assertEquals(10000, $ca->getSalario());
+     }
+   	public function testSetSectorFuncionaBien() {
+    	$ca = $this->crear();
+    	$ca->setSector("Informatica");
 	    $this->assertEquals("Informatica",$ca->getSector());
-	}
-        public function testToString() {
-		$ca = $this->crear();
-		$this->assertEquals("Fulano De Tal 11111111 10000", $ca->__toString());
-	}
-	public function testNombreEmpleadoVacio(){
-		$this->expectException(\Exception::class);
-		$ca = $this->crear(1000,[100,200],"","" );
-	}	
-	public function testDNIVacio(){
-		$this->expectException(\Exception::class);
-		$ca = $this->crear(1000,[100,200],"Fulano","De Tal","");
-	}
-	public function testSalarioVacio(){
-		$this->expectException(\Exception::class);
-		$ca = $this->crear("",[100,200]);
-	}
-}
+	  }
+    public function testToString() {
+      $ca = $this->crear();
+      $this->assertEquals("Fulano De Tal 11111111 10000", $ca->__toString());
+    }
+    public function testNombreEmpleadoVacio(){
+      $this->expectException(\Exception::class);
+      $ca = $this->crear(1000,[100,200],"","" );
+    }	
+    public function testDNIVacio(){
+      $this->expectException(\Exception::class);
+      $ca = $this->crear(1000,[100,200],"Fulano","De Tal","");
+    }
+    public function testSalarioVacio(){
+      $this->expectException(\Exception::class);
+      $ca = $this->crear("",[100,200]);
+    }
+    public function testValidacionDNI(){
+      $this->expectException(\Exception::class);
+      $ca = $this->crear(1000,[100,200],"Fulano","De Tal","11111a1");
+    }
+    public function testNoEspecificadoSector(){
+      $ca = $this->crear();
+      $ca->setSector("");
+      $this->assertEquals("No especificado",$ca->getSector());
+    }
+    
+  }
+
